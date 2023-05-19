42cce19671a9b25670346674dd0143765b261e3d
chinna <chinna@gmail.com>
2023-05-19 13:07:00 +0530

    installing eksctl & kubectl cli

---
+++ kubectl-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.4/2023-05-11/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+kubectl version --short --client
+
+
